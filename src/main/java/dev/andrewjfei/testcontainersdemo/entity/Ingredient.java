4f51b9438f11dd17e5ab2ca0d48965db649dcb16
Andrew J Fei <andrewjfei@gmail.com>
2022-09-05 14:40:15 +1200

    NONE - added ingredient entity

---
+++ src/main/java/dev/andrewjfei/testcontainersdemo/entity/Ingredient.java
@@ -0,0 +1,43 @@
+package dev.andrewjfei.testcontainersdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity( name = "ingredient")
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
